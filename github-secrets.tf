c3355829bd8d5c2fd3d74d373170c4ef62078a96
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-10-29 00:30:00 -0400

    updating presentation

--- github-secrets.tf
+++ github-secrets.tf
@@ -1,6 +1,6 @@
 resource "github_actions_secret" "ACR_LOGIN_SERVER" {
   #repository      = var.DOCS_BUILDER_REPO_NAME
-  repository = "amerintlxperts/docs-builder"
+  repository = "docs-builder"
   secret_name     = "ACR_LOGIN_SERVER"
   plaintext_value = azurerm_container_registry.container_registry.login_server
 }
