2fc4b5056b529f6b5b64ae8d676856119cc7dc56
AJLab-GH <ajammes.ftnt@gmail.com>
2024-10-31 11:44:03 -0600

    added proper secrets config

--- spoke-k8s_cluster.tf
+++ spoke-k8s_cluster.tf
@@ -253,9 +253,18 @@ resource "kubernetes_secret" "lacework_agent_token" {
     namespace = kubernetes_namespace.lacework.metadata[0].name
   }
   data = {
-    token = var.LW_AGENT_TOKEN
+    "config.json" = jsonencode({
+      tokens = {
+        AccessToken = var.LW_AGENT_TOKEN
+      },
+      serverurl = "https://api.lacework.net",
+      tags = {
+        Env              = "k8s",
+        KubernetesCluster = azurerm_kubernetes_cluster.kubernetes_cluster.name
+      }
+    }),
+    "syscall_config.yaml" = ""
   }
-  type = "Opaque"
 }
 
 locals {
