5c6904c4c380fcc55a186b966d71b685e3eefb85
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-11-03 14:51:51 -0500

    fix: updated azure login

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
       publisher = "fortinet"
       offer     = "fortinet_fortiweb-vm_v5"
       size      = "Standard_F16s_v2"
-      size-dev  = "Standard_F16s_v2"
+      size-dev  = "Standard_D3_v2"
       version   = "latest"
       #sku             = "fortinet_fw-vm_payg_v3"
       sku = "fortinet_fw-vm_payg_v2"
@@ -21,11 +21,8 @@ locals {
       size-dev  = "Standard_B8ms"
       #size-dev = "Standard_B2ms"
       gpu-size = "Standard_NC24s_v3"
-      gpu-size-dev = "Standard_NC24s_v3"
+      gpu-size-dev = "Standard_NC4as_T4_v3"
       #gpu-size  = "Standard_NC6s_v3" #16GB
-      #gpu-size      = "Standard_NC4as_T4_v3" # 16GB
-      #gpu-size      = "Standard_ND40rs_v2" # 32 GB vlink
-      #gpu-size      = "Standard_NC24ads_A100_v4" # 80GB - not supported by azure-linux
     }
   }
 }
