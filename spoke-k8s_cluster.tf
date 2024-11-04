ade3939b33b308b8c1c5f2920a4d11b1e787cf4a
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-11-03 23:48:16 -0500

    fix: updated azure login

--- spoke-k8s_cluster.tf
+++ spoke-k8s_cluster.tf
@@ -35,7 +35,8 @@ resource "azurerm_container_registry" "container_registry" {
   name                          = random_string.acr_name.result
   resource_group_name           = azurerm_resource_group.azure_resource_group.name
   location                      = azurerm_resource_group.azure_resource_group.location
-  sku                           = "Premium"
+  #sku                           = var.ENVIRONMENT_GRADE == "Production" ? "Standard" : "Basic"
+  sku                           = "Basic"
   admin_enabled                 = false
   public_network_access_enabled = true
   anonymous_pull_enabled        = false
