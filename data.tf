be0fafd1d7a58ddb8e58d2752566df7e92b42390
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-11-07 22:02:52 -0500

    feat: seaprate manifests repos

--- data.tf
+++ data.tf
@@ -1,5 +1,20 @@
-data "azurerm_public_ip" "hub-nva-vip_public_ip" {
-  name                = azurerm_public_ip.hub-nva-vip_public_ip.name
+data "azurerm_public_ip" "hub-nva-vip_docs_public_ip" {
+  name                = azurerm_public_ip.hub-nva-vip_docs_public_ip.name
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+}
+
+data "azurerm_public_ip" "hub-nva-vip_dvwa_public_ip" {
+  name                = azurerm_public_ip.hub-nva-vip_dvwa_public_ip.name
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+}
+
+data "azurerm_public_ip" "hub-nva-vip_ollama_public_ip" {
+  name                = azurerm_public_ip.hub-nva-vip_ollama_public_ip.name
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+}
+
+data "azurerm_public_ip" "hub-nva-vip_video_public_ip" {
+  name                = azurerm_public_ip.hub-nva-vip_video_public_ip.name
   resource_group_name = azurerm_resource_group.azure_resource_group.name
 }
 
