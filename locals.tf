517207aa8812b01699843113c525eb7805752418
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-11-12 14:15:53 -0500

    adding lower thirds

--- locals.tf
+++ locals.tf
@@ -4,10 +4,8 @@ locals {
       publisher = "fortinet"
       offer     = "fortinet_fortiweb-vm_v5"
       size      = "Standard_F16s_v2"
-      size-dev      = "Standard_F16s_v2"
-      #size-dev  = "Standard_D3_v2"
+      size-dev  = "Standard_D3_v2"
       version   = "latest"
-      #sku             = "fortinet_fw-vm_payg_v3"
       sku             = "fortinet_fw-vm_payg_v2"
       management-port = "8443"
       terms           = true
@@ -23,7 +21,6 @@ locals {
       #size-dev = "Standard_B2ms"
       gpu-size     = "Standard_NC24s_v3"
       gpu-size-dev = "Standard_NC4as_T4_v3"
-      #gpu-size  = "Standard_NC6s_v3" #16GB
     }
   }
 }
