0b036240389569221b69486f28aeb63877a000ca
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-11-13 08:57:01 -0500

    Update hub-nva.tf

--- hub-nva.tf
+++ hub-nva.tf
@@ -2,60 +2,60 @@ resource "azurerm_network_interface" "hub-nva-external_network_interface" {
   name                           = "hub-nva-external_network_interface"
   location                       = azurerm_resource_group.azure_resource_group.location
   resource_group_name            = azurerm_resource_group.azure_resource_group.name
-  accelerated_networking_enabled = var.PRODUCTION_ENVIRONMENT ? true : false
+  accelerated_networking_enabled = true
+  dynamic "ip_configuration" {
+    for_each = [
+      {
+        name                          = "hub-nva-external-management_ip_configuration"
+        primary                       = true
+        private_ip_address_allocation = "Static"
+        private_ip_address            = var.hub-nva-management-ip
+        subnet_id                     = azurerm_subnet.hub-external_subnet.id
+        public_ip_address_id          = var.PRODUCTION_ENVIRONMENT ? null : azurerm_public_ip.hub-nva-management_public_ip[0].id
+      },
+      {
+        name                          = "hub-nva-external-vip-docs_configuration"
+        primary                       = false
+        private_ip_address_allocation = "Static"
+        private_ip_address            = var.hub-nva-vip-docs
+        subnet_id                     = azurerm_subnet.hub-external_subnet.id
+        public_ip_address_id          = azurerm_public_ip.hub-nva-vip_docs_public_ip[0].id
+      },
+      {
+        name                          = "hub-nva-external-vip-dvwa_configuration"
+        primary                       = false
+        private_ip_address_allocation = "Static"
+        private_ip_address            = var.hub-nva-vip-dvwa
+        subnet_id                     = azurerm_subnet.hub-external_subnet.id
+        public_ip_address_id          = azurerm_public_ip.hub-nva-vip_dvwa_public_ip[0].id
+      },
+      {
+        name                          = "hub-nva-external-vip-ollama_configuration"
+        primary                       = false
+        private_ip_address_allocation = "Static"
+        private_ip_address            = var.hub-nva-vip-ollama
+        subnet_id                     = azurerm_subnet.hub-external_subnet.id
+        public_ip_address_id          = azurerm_public_ip.hub-nva-vip_ollama_public_ip[0].id
+      },
+      {
+        name                          = "hub-nva-external-vip-video_configuration"
+        primary                       = false
+        private_ip_address_allocation = "Static"
+        private_ip_address            = var.hub-nva-vip-video
+        subnet_id                     = azurerm_subnet.hub-external_subnet.id
+        public_ip_address_id          = azurerm_public_ip.hub-nva-vip_video_public_ip[0].id
+      }
+    ]
 
-  ip_configuration {
-    name                          = "hub-nva-external-management_ip_configuration"
-    primary                       = true
-    private_ip_address_allocation = "Static"
-    private_ip_address            = var.hub-nva-management-ip
-    subnet_id                     = azurerm_subnet.hub-external_subnet.id
-    public_ip_address_id          = var.PRODUCTION_ENVIRONMENT ? null : azurerm_public_ip.hub-nva-management_public_ip[0].id
+    content {
+      name                          = ip_configuration.value.name
+      primary                       = lookup(ip_configuration.value, "primary", false)
+      private_ip_address_allocation = ip_configuration.value.private_ip_address_allocation
+      private_ip_address            = ip_configuration.value.private_ip_address
+      subnet_id                     = ip_configuration.value.subnet_id
+      public_ip_address_id          = ip_configuration.value.public_ip_address_id
+    }
   }
-
-#  dynamic "ip_configuration" {
-#    for_each = var.APPLICATION_DOCS ? ["docs"] : []
-#    content {
-#      name                          = "hub-nva-external-vip-docs_configuration"
-#      private_ip_address_allocation = "Static"
-#      private_ip_address            = var.hub-nva-vip-docs
-#      subnet_id                     = azurerm_subnet.hub-external_subnet.id
-#      public_ip_address_id          = azurerm_public_ip.hub-nva-vip_docs_public_ip[0].id #checkov:skip=CKV_AZURE_119:Fortigate gets a public IP
-#    }
-#  }
-
-#  dynamic "ip_configuration" {
-#    for_each = var.APPLICATION_DVWA ? ["dvwa"] : []
-#    content {
-#      name                          = "hub-nva-external-vip-dvwa_configuration"
-#      private_ip_address_allocation = "Static"
-#      private_ip_address            = var.hub-nva-vip-dvwa
-#      subnet_id                     = azurerm_subnet.hub-external_subnet.id
-#      public_ip_address_id          = azurerm_public_ip.hub-nva-vip_dvwa_public_ip[0].id #checkov:skip=CKV_AZURE_119:Fortigate gets a public IP
-#    }
-#  }
-
-#  dynamic "ip_configuration" {
-#    for_each = var.APPLICATION_OLLAMA ? ["ollama"] : []
-#    content {
-#      name                          = "hub-nva-external-vip-ollama_configuration"
-#      private_ip_address_allocation = "Static"
-#      private_ip_address            = var.hub-nva-vip-ollama
-#      subnet_id                     = azurerm_subnet.hub-external_subnet.id
-#      public_ip_address_id          = azurerm_public_ip.hub-nva-vip_ollama_public_ip[0].id #checkov:skip=CKV_AZURE_119:Fortigate gets a public IP
-#    }
-#  }
-
-#  dynamic "ip_configuration" {
-#    for_each = var.APPLICATION_VIDEO ? ["video"] : []
-#    content {
-#      name                          = "hub-nva-external-vip-video_configuration"
-#      private_ip_address_allocation = "Static"
-#      private_ip_address            = var.hub-nva-vip-video
-#      subnet_id                     = azurerm_subnet.hub-external_subnet.id
-#      public_ip_address_id          = azurerm_public_ip.hub-nva-vip_video_public_ip[0].id #checkov:skip=CKV_AZURE_119:Fortigate gets a public IP
-#    }
-#  }
 }
 
 resource "azurerm_network_interface" "hub-nva-internal_network_interface" {
@@ -76,7 +76,7 @@ resource "azurerm_linux_virtual_machine" "hub-nva_virtual_machine" {
   #checkov:skip=CKV_AZURE_178: Allow Fortigate to present HTTPS login UI instead of SSH
   #checkov:skip=CKV_AZURE_149: Allow Fortigate to present HTTPS login UI instead of SSH
   #checkov:skip=CKV_AZURE_1: Allow Fortigate to present HTTPS login UI instead of SSH
-  #depends_on                      = [null_resource.marketplace_agreement, azurerm_managed_disk.log_disk]
+  depends_on                      = [null_resource.marketplace_agreement, azurerm_managed_disk.log_disk]
   name                            = "hub-nva_virtual_machine"
   computer_name                   = "hub-nva"
   availability_set_id             = azurerm_availability_set.hub-nva_availability_set.id
@@ -88,13 +88,14 @@ resource "azurerm_linux_virtual_machine" "hub-nva_virtual_machine" {
   network_interface_ids           = [azurerm_network_interface.hub-nva-external_network_interface.id, azurerm_network_interface.hub-nva-internal_network_interface.id]
   size                            = var.PRODUCTION_ENVIRONMENT ? local.vm-image[var.hub-nva-image].size : local.vm-image[var.hub-nva-image].size-dev
   allow_extension_operations      = false
-#  identity {
-#    type = "SystemAssigned"
-#  }
+
+  identity {
+    type = "SystemAssigned"
+  }
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = var.PRODUCTION_ENVIRONMENT ? "Premium_LRS" : "Standard_LRS"
-    disk_size_gb         = var.PRODUCTION_ENVIRONMENT ? 256 : 128
+    #disk_size_gb = var.PRODUCTION_ENVIRONMENT ? 256 : 128
   }
   plan {
     name      = local.vm-image[var.hub-nva-image].sku
@@ -107,29 +108,29 @@ resource "azurerm_linux_virtual_machine" "hub-nva_virtual_machine" {
     sku       = local.vm-image[var.hub-nva-image].sku
     version   = "latest"
   }
-#  custom_data = base64encode(
-#    templatefile("cloud-init/${var.hub-nva-image}.conf",
-#      {
-#        VAR-hub-external-subnet-gateway          = var.hub-external-subnet-gateway
-#        VAR-spoke-check-internet-up-ip           = var.spoke-check-internet-up-ip
-#        VAR-spoke-default-gateway                = cidrhost(var.hub-internal-subnet_prefix, 1)
-#        VAR-spoke-virtual-network_address_prefix = var.spoke-virtual-network_address_prefix
-#        VAR-spoke-virtual-network_subnet         = cidrhost(var.spoke-virtual-network_address_prefix, 0)
-#        VAR-spoke-virtual-network_netmask        = cidrnetmask(var.spoke-virtual-network_address_prefix)
-#        VAR-spoke-aks-node-ip                    = var.spoke-aks-node-ip
-#        VAR-hub-nva-vip-docs                     = var.hub-nva-vip-docs
-#        VAR-hub-nva-vip-ollama                   = var.hub-nva-vip-ollama
-#        VAR-hub-nva-vip-video                    = var.hub-nva-vip-video
-#        VAR-hub-nva-vip-dvwa                     = var.hub-nva-vip-dvwa
-#        VAR-HUB_NVA_USERNAME                     = var.HUB_NVA_USERNAME
-#        VAR-CERTIFICATE                          = tls_self_signed_cert.self_signed_cert.cert_pem
-#        VAR-PRIVATEKEY                           = tls_private_key.private_key.private_key_pem
-#        VAR-fwb_license_file                     = ""
-#        VAR-fwb_license_fortiflex                = ""
-#        VAR-spoke-aks-network                    = var.spoke-aks-subnet_prefix
-#      }
-#    )
-#  )
+  custom_data = base64encode(
+    templatefile("cloud-init/${var.hub-nva-image}.conf",
+      {
+        VAR-hub-external-subnet-gateway          = var.hub-external-subnet-gateway
+        VAR-spoke-check-internet-up-ip           = var.spoke-check-internet-up-ip
+        VAR-spoke-default-gateway                = cidrhost(var.hub-internal-subnet_prefix, 1)
+        VAR-spoke-virtual-network_address_prefix = var.spoke-virtual-network_address_prefix
+        VAR-spoke-virtual-network_subnet         = cidrhost(var.spoke-virtual-network_address_prefix, 0)
+        VAR-spoke-virtual-network_netmask        = cidrnetmask(var.spoke-virtual-network_address_prefix)
+        VAR-spoke-aks-node-ip                    = var.spoke-aks-node-ip
+        VAR-hub-nva-vip-docs                     = var.hub-nva-vip-docs
+        VAR-hub-nva-vip-ollama                   = var.hub-nva-vip-ollama
+        VAR-hub-nva-vip-video                    = var.hub-nva-vip-video
+        VAR-hub-nva-vip-dvwa                     = var.hub-nva-vip-dvwa
+        VAR-HUB_NVA_USERNAME                     = var.HUB_NVA_USERNAME
+        VAR-CERTIFICATE                          = tls_self_signed_cert.self_signed_cert.cert_pem
+        VAR-PRIVATEKEY                           = tls_private_key.private_key.private_key_pem
+        VAR-fwb_license_file                     = ""
+        VAR-fwb_license_fortiflex                = ""
+        VAR-spoke-aks-network                    = var.spoke-aks-subnet_prefix
+      }
+    )
+  )
 }
 
 resource "azurerm_managed_disk" "log_disk" {
@@ -141,9 +142,9 @@ resource "azurerm_managed_disk" "log_disk" {
   disk_size_gb         = var.PRODUCTION_ENVIRONMENT ? 128 : 36
 }
 
-#resource "azurerm_virtual_machine_data_disk_attachment" "log_disk" {
-#  managed_disk_id    = azurerm_managed_disk.log_disk.id
-#  virtual_machine_id = azurerm_linux_virtual_machine.hub-nva_virtual_machine.id
-#  lun                = "0"
-#  caching            = "ReadWrite"
-#}
+resource "azurerm_virtual_machine_data_disk_attachment" "log_disk" {
+  managed_disk_id    = azurerm_managed_disk.log_disk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.hub-nva_virtual_machine.id
+  lun                = "0"
+  caching            = "ReadWrite"
+}
