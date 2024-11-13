3ff14eed26cad5a82bfe3ceb9f694ed03761b4aa
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-11-13 10:19:05 -0500

    feat: seaprate manifests repos

--- terraform.tf
+++ terraform.tf
@@ -73,4 +73,5 @@ provider "kubernetes" {
   client_certificate     = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config[0].cluster_ca_certificate)
+  config_path            = "~/.kube/config"
 }
