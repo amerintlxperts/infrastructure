9c0a20b7c85c4daec2026a9185725c10f5f62139
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-11-22 08:06:39 -0500

    add sample feature

--- hub-network.tf
+++ hub-network.tf
@@ -1,4 +1,4 @@
-esource "azurerm_dns_zone" "dns_zone" {
+resource "azurerm_dns_zone" "dns_zone" {
   name                = var.DNS_ZONE
   resource_group_name = azurerm_resource_group.azure_resource_group.name
 }
