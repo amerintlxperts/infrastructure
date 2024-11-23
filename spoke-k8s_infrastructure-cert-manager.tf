de88c75f541bab9caf04437f20fb234f4bb54e05
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-11-23 14:19:18 -0500

    foo

--- spoke-k8s_infrastructure-cert-manager.tf
+++ spoke-k8s_infrastructure-cert-manager.tf
@@ -31,6 +31,7 @@ resource "azurerm_user_assigned_identity" "cert-manager" {
 data "azurerm_user_assigned_identity" "cert_manager_data" {
   name                = azurerm_user_assigned_identity.cert-manager.name
   resource_group_name = azurerm_resource_group.azure_resource_group.name
+  depends_on = azurerm_user_assigned_identity.cert-manager
 }
 
 resource "azurerm_role_assignment" "cert-manager_role_assignment" {
@@ -81,6 +82,7 @@ resource "azurerm_kubernetes_flux_configuration" "cert-manager" {
     depends_on                 = ["cert-manager"]
   }
   depends_on = [
-    azurerm_kubernetes_cluster_extension.flux_extension
+    azurerm_kubernetes_cluster_extension.flux_extension,
+    kubernetes_namespace.cert-manager
   ]
 }
